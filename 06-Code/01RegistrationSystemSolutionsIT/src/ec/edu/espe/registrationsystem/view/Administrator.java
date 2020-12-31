006af6c078a0d909ccc2de55a3dc5464ce903fdb
Nathaly Melissa Vivas Pin <75291166+nmvivas@users.noreply.github.com>
2020-12-31 12:40:43 -0500

    Code Netbeans.version0

---
+++ 06-Code/01RegistrationSystemSolutionsIT/src/ec/edu/espe/registrationsystem/view/Administrator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.registrationsystem.view;
+
+/**
+ *
+ * @author NATALIA
+ */
+public class Administrator {
+    private Integer cedula;
+    private String nombres;
+    private String apellidos;
+    private String email;
+    private String nickname;
+    private Integer password;
+    
+}
