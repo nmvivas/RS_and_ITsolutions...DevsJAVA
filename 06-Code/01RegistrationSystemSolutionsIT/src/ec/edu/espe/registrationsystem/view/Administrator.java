a43785fa3416a794594b3a9075ec6cdaa8b50dee
Karen Alejandra Velasco Guanoluisa <64505621+kavelasco1@users.noreply.github.com>
2021-01-01 18:50:44 -0500

    setter of class Adminstrator

--- 06-Code/01RegistrationSystemSolutionsIT/src/ec/edu/espe/registrationsystem/view/Administrator.java
+++ 06-Code/01RegistrationSystemSolutionsIT/src/ec/edu/espe/registrationsystem/view/Administrator.java
@@ -50,6 +50,30 @@ public class Administrator {
     public Integer getPassword() {
         return password;
     }
+
+    public void setIdCard(Integer idCard) {
+        this.idCard = idCard;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setPassword(Integer password) {
+        this.password = password;
+    }
     
     
     
