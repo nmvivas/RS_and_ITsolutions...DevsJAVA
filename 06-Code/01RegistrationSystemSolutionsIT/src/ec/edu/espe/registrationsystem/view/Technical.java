fee609cf3e939c021600e83cc935ccaca44fb125
Nathaly Melissa Vivas Pin <75291166+nmvivas@users.noreply.github.com>
2021-01-01 16:39:57 -0500

    Add attributes to Technical

--- 06-Code/01RegistrationSystemSolutionsIT/src/ec/edu/espe/registrationsystem/view/Technical.java
+++ 06-Code/01RegistrationSystemSolutionsIT/src/ec/edu/espe/registrationsystem/view/Technical.java
@@ -10,5 +10,12 @@ package ec.edu.espe.registrationsystem.view;
  * @author NATALIA
  */
 public class Technical {
+    private Integer idCard;
+    private String names;
+    private String lastnames;
+    private Integer telephone;
+    private String email;
+    private String position;
+    private String assignedLevel;
     
 }
