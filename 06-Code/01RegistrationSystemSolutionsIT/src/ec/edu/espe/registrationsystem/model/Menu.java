841775ab0db6b5313403d51880104dd25e0a3d71
CharlesZambrano <charleszambranoespe@outlook.com>
2021-01-12 19:30:39 -0500

    Add opcion3 in Class Menu

--- 06-Code/01RegistrationSystemSolutionsIT/src/ec/edu/espe/registrationsystem/model/Menu.java
+++ 06-Code/01RegistrationSystemSolutionsIT/src/ec/edu/espe/registrationsystem/model/Menu.java
@@ -13,19 +13,24 @@ public class Menu {
     
     private Integer opcion1;
     private Integer opcion2;
+    private Integer opcion3;
     
-    public Menu(Integer opcion1, Integer opcion2){
+    public Menu(Integer opcion1, Integer opcion2, Integer opcion3){
         
         this.opcion1 = opcion1;
         this.opcion2 = opcion2;
+        this.opcion3 = opcion3;
         
-
+     System.out.println(" Welcome to the System ");
+     System.out.println(" Select 1 : If you want to create a user administrator ");
+     System.out.println(" Select 2 : If you want to login ");
         
 }
 
     @Override
     public String toString() {
-        return "Menu{" + "opcion1=" + opcion1 + ", opcion2=" + opcion2 + '}';
+        return "Menu{" + "opcion1=" + opcion1 + ", opcion2=" + opcion2 + 
+                ", opcion3=" + opcion3 + '}';
     }
 
     public Integer getOpcion1() {
@@ -43,6 +48,16 @@ public class Menu {
     public void setOpcion2(Integer opcion2) {
         this.opcion2 = opcion2;
     }
+
+    public Integer getOpcion3() {
+        return opcion3;
+    }
+
+    public void setOpcion3(Integer opcion3) {
+        this.opcion3 = opcion3;
+    }
+    
+   
 
 }
 
