cb236020e26488bc5882925812b69109f2c06f8c
CharlesZambrano <charleszambranoespe@outlook.com>
2021-02-04 18:54:28 -0500

    Add refactored File Class with method getDayOfAttention

---
+++ 06-Code/RegistrationSystemSolutionIT/src/ec/edu/espe/registrationsystemsolution/model/File.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.registrationsystemsolution.model;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+/**
+ *
+ * @author CHARLES
+ */
+public class File {
+    
+    private int idProblem;
+    private String titleProblem;
+    private LocalDate dateOfShipment;
+
+    public int getDayOfAttention() {
+        int age = 0;
+        LocalDate currentDate;
+
+        currentDate = LocalDate.now();
+        age = Period.between(dateOfShipment, currentDate).getYears()
+                + Period.between(dateOfShipment, currentDate).getMonths()
+                + Period.between(dateOfShipment, currentDate).getDays();
+        return age;
+    }
+
+    public File(int idProblem, String titleProblem, LocalDate dateOfShipment) {
+        this.idProblem = idProblem;
+        this.titleProblem = titleProblem;
+        this.dateOfShipment = dateOfShipment;
+    }
+
+    @Override
+    public String toString() {
+        return " idProblem(" + idProblem + ") titleProblem "
+                + "(" + titleProblem + ") dateOfShipment (" + dateOfShipment + ')';
+    }
+
+    /**
+     * @return the idProblem
+     */
+    public int getIdProblem() {
+        return idProblem;
+    }
+
+    /**
+     * @param idProblem the idProblem to set
+     */
+    public void setIdProblem(int idProblem) {
+        this.idProblem = idProblem;
+    }
+
+    /**
+     * @return the titleProblem
+     */
+    public String getTitleProblem() {
+        return titleProblem;
+    }
+
+    /**
+     * @param titleProblem the titleProblem to set
+     */
+    public void setTitleProblem(String titleProblem) {
+        this.titleProblem = titleProblem;
+    }
+
+    /**
+     * @return the dateOfShipment
+     */
+    public LocalDate getDateOfShipment() {
+        return dateOfShipment;
+    }
+
+    /**
+     * @param dateOfShipment the dateOfShipment to set
+     */
+    public void setDateOfShipment(LocalDate dateOfShipment) {
+        this.dateOfShipment = dateOfShipment;
+    }
+    
+}
