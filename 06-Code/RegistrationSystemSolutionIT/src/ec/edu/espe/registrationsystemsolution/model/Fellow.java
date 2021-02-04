d8b133351b3d4e7c5e41bab740a4cc9b8d8c8492
CharlesZambrano <charleszambranoespe@outlook.com>
2021-02-04 18:39:55 -0500

    Add new Java Class Fellow to .model

---
+++ 06-Code/RegistrationSystemSolutionIT/src/ec/edu/espe/registrationsystemsolution/model/Fellow.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.registrationsystemsolution.model;
+
+import ec.edu.espe.filemanager.utils.Data;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DEVS_JAVA_KND
+ */
+public class Fellow {
+    
+    private int idCard;
+    private String names;
+    private String surnames;
+
+    public Fellow(int idCard, String names, String surnames) {
+        this.idCard = idCard;
+        this.names = names;
+        this.surnames = surnames;
+    }
+
+    @Override
+    public String toString() {
+        return "Fellow{" + "idCard = " + idCard + ", names = " + names
+                + ", surnames = " + surnames + '}';
+    }
+
+    public void saveFellow(boolean option, String informationData) {
+        if (option == true) {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter file Name and extension (.csv - .txt - .gson )---> ");
+            String filename = scan.nextLine();
+            Data.save(filename, informationData);
+            System.out.println(" \n *------- SAVED FILE ----- * ");
+        } else {
+            System.out.println(" --- EXIT THE SYSTEM --- ");
+            System.exit(0);
+        }
+
+    }
+
+    /**
+     * @return the idCard
+     */
+    public int getIdCard() {
+        return idCard;
+    }
+
+    /**
+     * @param idCard the idCard to set
+     */
+    public void setIdCard(int idCard) {
+        this.idCard = idCard;
+    }
+
+    /**
+     * @return the names
+     */
+    public String getNames() {
+        return names;
+    }
+
+    /**
+     * @param names the names to set
+     */
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    /**
+     * @return the surnames
+     */
+    public String getSurnames() {
+        return surnames;
+    }
+
+    /**
+     * @param surnames the surnames to set
+     */
+    public void setSurnames(String surnames) {
+        this.surnames = surnames;
+    }
+}
