5825ba95532d9f190688f03bc591c501cf9f3947
CharlesZambrano <charleszambranoespe@outlook.com>
2021-02-04 18:56:37 -0500

    Add refactored Problem Class with inheritance and new method archiveProblem

---
+++ 06-Code/RegistrationSystemSolutionIT/src/ec/edu/espe/registrationsystemsolution/model/Problem.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.registrationsystemsolution.model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author CHARLES
+ */
+public class Problem extends File {
+
+    private boolean isSolve;
+    private static int problemInTheBaseData;
+
+    public Problem(boolean isSolve, int idProblem, String titleProblem, LocalDate dateOfShipment) {
+        super(idProblem, titleProblem, dateOfShipment);
+        this.isSolve = isSolve;
+    }
+
+    public int archiveProblem() {
+        if (isSolve == false) {
+            problemInTheBaseData++;
+            return problemInTheBaseData;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * @return the isSolve
+     */
+    public boolean isIsSolve() {
+        return isSolve;
+    }
+
+    /**
+     * @param isSolve the isSolve to set
+     */
+    public void setIsSolve(boolean isSolve) {
+        this.isSolve = isSolve;
+    }
+
+    public int getProblemInTheBaseData() {
+        return problemInTheBaseData;
+    }
+
+    public void setProblemInTheBaseData(int problemInTheBaseData) {
+        Problem.problemInTheBaseData = problemInTheBaseData;
+    }
+    
+}
+
