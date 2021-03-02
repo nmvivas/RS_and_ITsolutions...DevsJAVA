7f4ffcea00290be7b9cacf0e697cf07370dd40fe
Karen Alejandra Velasco Guanoluisa <64505621+kavelasco1@users.noreply.github.com>
2021-03-01 22:47:35 -0500

    Create Class Verification for the User in password

---
+++ 06-Code/RegistrationSystemSolutionIT/src/ec/edu/espe/registrationsystemsolution/model/Verification.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.registrationsystemsolution.model;
+
+import ec.edu.espe.registrationsystemsolution.view.FrmLogin;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author karen
+ */
+public class Verification {
+    FrmLogin login=  new FrmLogin();
+    String res;
+    
+    public void VerfiyUser(String users[], String user, String password, int attempts) {
+        boolean findOut = false;
+
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].equalsIgnoreCase(user) && users[i + 1].equals(password)) {
+                res = "Bienvenido ---> " + user;
+                findOut = true;
+                JOptionPane.showMessageDialog(null, res, "LOGIN", JOptionPane.INFORMATION_MESSAGE);
+                attempts=0;
+
+                break;
+            } 
+        }
+        if (findOut == false) {
+            res = " Key y/o users incorrects ... go " + attempts + " attempts wrong";
+            JOptionPane.showMessageDialog(null, res, "LOGIN", JOptionPane.ERROR_MESSAGE);
+        }
+        if (attempts > 2) {
+            JOptionPane.showMessageDialog(null, "3 attempts Wrongs ", "LOGIN", JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+
+        }
+    }
+}
