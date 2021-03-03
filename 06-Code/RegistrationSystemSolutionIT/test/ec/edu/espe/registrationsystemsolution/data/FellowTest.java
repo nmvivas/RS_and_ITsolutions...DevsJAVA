6e7c42a9af9bfe94c3dae4ae6b129b8c7559782d
RUBENTOMALA <ruben.al97@hotmail.com>
2021-03-03 04:15:56 -0500

    Unit test of Fellow and FIle, added

---
+++ 06-Code/RegistrationSystemSolutionIT/test/ec/edu/espe/registrationsystemsolution/data/FellowTest.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.registrationsystemsolution.data;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ruben
+ */
+public class FellowTest {
+    
+    public FellowTest() {
+    }
+
+    /**
+     * Test of toString method, of class Fellow.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Fellow instance = null;
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getIdCard method, of class Fellow.
+     */
+    @Test
+    public void testGetIdCard() {
+        System.out.println("getIdCard");
+        Fellow instance = null;
+        String expResult = "";
+        String result = instance.getIdCard();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setIdCard method, of class Fellow.
+     */
+    @Test
+    public void testSetIdCard() {
+        System.out.println("setIdCard");
+        String idCard = "";
+        Fellow instance = null;
+        instance.setIdCard(idCard);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getName method, of class Fellow.
+     */
+    @Test
+    public void testGetName() {
+        System.out.println("getName");
+        Fellow instance = null;
+        String expResult = "";
+        String result = instance.getName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setName method, of class Fellow.
+     */
+    @Test
+    public void testSetName() {
+        System.out.println("setName");
+        String name = "";
+        Fellow instance = null;
+        instance.setName(name);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSurname method, of class Fellow.
+     */
+    @Test
+    public void testGetSurname() {
+        System.out.println("getSurname");
+        Fellow instance = null;
+        String expResult = "";
+        String result = instance.getSurname();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSurname method, of class Fellow.
+     */
+    @Test
+    public void testSetSurname() {
+        System.out.println("setSurname");
+        String surname = "";
+        Fellow instance = null;
+        instance.setSurname(surname);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTelephone method, of class Fellow.
+     */
+    @Test
+    public void testGetTelephone() {
+        System.out.println("getTelephone");
+        Fellow instance = null;
+        int expResult = 0;
+        int result = instance.getTelephone();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setTelephone method, of class Fellow.
+     */
+    @Test
+    public void testSetTelephone() {
+        System.out.println("setTelephone");
+        int telephone = 0;
+        Fellow instance = null;
+        instance.setTelephone(telephone);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
