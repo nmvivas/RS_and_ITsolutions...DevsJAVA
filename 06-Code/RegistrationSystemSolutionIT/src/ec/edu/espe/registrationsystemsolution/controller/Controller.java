c3b67d9bf92f80b31f0755b3943ba51bce9e7d7e
Nathaly Melissa Vivas Pin <meliivp01@gmail.com>
2021-03-03 03:24:39 -0500

    Creation the class Controller in the package controller

--- 06-Code/RegistrationSystemSolutionIT/src/ec/edu/espe/registrationsystemsolution/controller/Controller.java
+++ 06-Code/RegistrationSystemSolutionIT/src/ec/edu/espe/registrationsystemsolution/controller/Controller.java
@@ -6,6 +6,7 @@
 package ec.edu.espe.registrationsystemsolution.controller;
 
 import ec.edu.espe.registrationsystemsolution.data.AdminList;
+import ec.edu.espe.registrationsystemsolution.model.UserList;
 import ec.edu.espe.registrationsystemsolution.view.FrmLogin;
 import ec.edu.espe.registrationsystemsolution.view.FrmNewRequest;
 import java.awt.event.ActionEvent;
@@ -15,60 +16,76 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
- * @author Meli
+ * @author DEVS_JAVA_KND
  */
 public class Controller implements ActionListener, KeyListener, MouseListener, MouseMotionListener {
 
     FrmLogin login;
     FrmNewRequest newRequest;
+    UserList userList;
     AdminList adminList;
 
-    public Controller(FrmLogin login, FrmNewRequest newRequest, AdminList adminList) {
+    public Controller(FrmLogin login, FrmNewRequest newRequest, UserList userList, AdminList adminList) {
         this.login = login;
         this.newRequest = newRequest;
+        this.userList = userList;
         this.adminList = adminList;
 
-        
-    login.getButtonCancelar().addActionListener(this);
-    login.getButtonLogin().addActionListener(this);
-    
-   
-    
-        
-        
-        
-        
-        
+        //LOGIN    
+        login.getButtonExit().addActionListener(this);
+        login.getButtonLogin().addActionListener(this);
+
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        if (ae.getSource() == login.getButtonCancelar()) {
+        if (ae.getSource() == login.getButtonLogin()) {
             login.getjLabel1().setVisible(true);
             boolean aux = false;
             String username1 = login.getTxtUser().getText();
             String password1 = login.getTxtPassword().getText();
-            adminList = new AdminList();
-            adminList.readA();
-            for (int i = 0; i < adminList.getAdminList().size(); i++) {
-                if (username1.equals(adminList.getAdminList().get(i).getNames())
-                        && password1.equals(adminList.getAdminList().get(i).getSurnames())) {
+            userList = new UserList();
+            newRequest.setVisible(true);
+            newRequest.setLocationRelativeTo(null);
+            login.setVisible(false);
+            userList.readU();
+            for (int i = 0; i < userList.getUserList().size(); i++) {
+                if (username1.equals(userList.getUserList().get(i).getUsername())
+                        && password1.equals(userList.getUserList().get(i).getPassword())) {
                     aux = true;
                     newRequest.getjLabel2().setText("Hola" + username1);
                     break;
                 } else {
                     aux = false;
-                    System.exit(0);
                 }
             }
-            if (aux == true) {
-                newRequest.setVisible(true);
-                newRequest.setLocationRelativeTo(null);
-                login.setVisible(false);
+        }
+
+        if (ae.getSource() == newRequest.getBtnShowRequest()) {
+            DefaultTableModel trequest = new DefaultTableModel();
+            trequest.addColumn("Customer");
+            trequest.addColumn("Type Problem");
+            trequest.addColumn("State");
+            trequest.addColumn("Technical");
+            newRequest.getTblContents().setModel(trequest);
+
+            String[] matriz = new String[8];
+            try {
+                matriz[0] = newRequest.getTxtCustomer().getText();
+                matriz[1] = newRequest.getChkRemote().getText();
+                matriz[2] = newRequest.getTxtState().getText();
+                matriz[3] = newRequest.getTxtTechnical().getText();
+                trequest.addRow(matriz);
+
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "ERROR");
             }
+
         }
 
     }
@@ -122,5 +139,4 @@ public class Controller implements ActionListener, KeyListener, MouseListener, M
     public void mouseMoved(MouseEvent me) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
 }
