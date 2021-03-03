78a86cc4189d13ce0a2db4cdbad66cf9fcf25306
RUBENTOMALA <ruben.al97@hotmail.com>
2021-03-03 04:17:13 -0500

    Unit test of User and Verification class, added.

---
+++ 06-Code/RegistrationSystemSolutionIT/test/ec/edu/espe/registrationsystemsolution/data/VerificationTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.registrationsystemsolution.data;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ruben
+ */
+public class VerificationTest {
+    
+    public VerificationTest() {
+    }
+
+    /**
+     * Test of VerfiyUser method, of class Verification.
+     */
+    @Test
+    public void testVerfiyUser() {
+        System.out.println("VerfiyUser");
+        String[] users = null;
+        String user = "";
+        String password = "";
+        int attempts = 0;
+        Verification instance = new Verification();
+        instance.VerfiyUser(users, user, password, attempts);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
