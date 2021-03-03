8031372105813a952010a0440ac3ed52c7f01b84
RUBENTOMALA <ruben.al97@hotmail.com>
2021-03-03 04:14:47 -0500

    A new Update, adden unit test to the class Administrator and AdminList

---
+++ 06-Code/RegistrationSystemSolutionIT/test/ec/edu/espe/registrationsystemsolution/data/AdministratorTest.java
@@ -0,0 +1,155 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.registrationsystemsolution.data;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ruben
+ */
+public class AdministratorTest {
+    
+    public AdministratorTest() {
+    }
+
+    /**
+     * Test of getEmail method, of class Administrator.
+     */
+    @Test
+    public void testGetEmail() {
+        System.out.println("getEmail");
+        Administrator instance = null;
+        String expResult = "";
+        String result = instance.getEmail();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setEmail method, of class Administrator.
+     */
+    @Test
+    public void testSetEmail() {
+        System.out.println("setEmail");
+        String email = "";
+        Administrator instance = null;
+        instance.setEmail(email);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getIdCard method, of class Administrator.
+     */
+    @Test
+    public void testGetIdCard() {
+        System.out.println("getIdCard");
+        Administrator instance = null;
+        String expResult = "";
+        String result = instance.getIdCard();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setIdCard method, of class Administrator.
+     */
+    @Test
+    public void testSetIdCard() {
+        System.out.println("setIdCard");
+        String idCard = "";
+        Administrator instance = null;
+        instance.setIdCard(idCard);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getNames method, of class Administrator.
+     */
+    @Test
+    public void testGetNames() {
+        System.out.println("getNames");
+        Administrator instance = null;
+        String expResult = "";
+        String result = instance.getNames();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setNames method, of class Administrator.
+     */
+    @Test
+    public void testSetNames() {
+        System.out.println("setNames");
+        String names = "";
+        Administrator instance = null;
+        instance.setNames(names);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSurnames method, of class Administrator.
+     */
+    @Test
+    public void testGetSurnames() {
+        System.out.println("getSurnames");
+        Administrator instance = null;
+        String expResult = "";
+        String result = instance.getSurnames();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSurnames method, of class Administrator.
+     */
+    @Test
+    public void testSetSurnames() {
+        System.out.println("setSurnames");
+        String surnames = "";
+        Administrator instance = null;
+        instance.setSurnames(surnames);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTelephone method, of class Administrator.
+     */
+    @Test
+    public void testGetTelephone() {
+        System.out.println("getTelephone");
+        Administrator instance = null;
+        int expResult = 0;
+        int result = instance.getTelephone();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setTelephone method, of class Administrator.
+     */
+    @Test
+    public void testSetTelephone() {
+        System.out.println("setTelephone");
+        int telephone = 0;
+        Administrator instance = null;
+        instance.setTelephone(telephone);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
