cea23704c429513d41b888b725b8582e8e2ae547
RUBEN TOMALA <ruben.al97@hotmail.com>
2021-03-03 03:14:05 -0500

    Creation the class Uuser and UserList for the connection

---
+++ 06-Code/RegistrationSystemSolutionIT/src/ec/edu/espe/registrationsystemsolution/data/User.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.registrationsystemsolution.data;
+
+import com.mongodb.BasicDBObject;
+
+/**
+ *
+ * @author Meli
+ */
+public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+    public User(BasicDBObject user){
+        username = user.getString("Username");
+        password = user.getString("Password");
+
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
---
+++ 06-Code/RegistrationSystemSolutionIT/src/ec/edu/espe/registrationsystemsolution/model/UserList.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.registrationsystemsolution.model;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import ec.edu.espe.registrationsystemsolution.data.ConnectionMongodb;
+import ec.edu.espe.registrationsystemsolution.data.User;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Meli
+ */
+public class UserList {
+
+    ArrayList<User> userList;
+    BasicDBObject document;
+    ConnectionMongodb connection;
+
+    public UserList() {
+        userList = new ArrayList();
+        connection = new ConnectionMongodb();
+        document = new BasicDBObject();
+    }
+
+    public ArrayList<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(ArrayList<User> userList) {
+        this.userList = userList;
+    }
+
+    public boolean insertU(User user) {
+        boolean aux = true;
+        document.append("Username", user.getUsername());
+        document.append("Password", user.getPassword());
+
+        for (int i = 0; i < userList.size(); i++) {
+            if (user.getUsername().equals(userList.get(i).getUsername())) {
+                aux = false;
+                break;
+            } else {
+                aux = true;
+            }
+        }
+        if (aux == true) {
+            connection.getDbCollection().insert(document);
+        }
+        return aux;
+    }
+
+    public boolean readU() {
+        boolean aux;
+        DBCursor cursor = connection.getDbCollection().find();
+        while (cursor.hasNext()) {
+            User user = new User((BasicDBObject) cursor.next());
+            userList.add(user);
+        }
+        aux = true;
+        return aux;
+    }
+
+    public boolean deleteList() {
+        boolean aux;
+        userList.clear();
+        aux = true;
+        return aux;
+    }
+}
